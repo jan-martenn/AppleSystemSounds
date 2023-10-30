a03731330e537885fb1d9e7b3a0362c7c435dd5c
jan-martenn <jan-marten@springtree.nl>
2023-10-30 12:16:41 +0100

    Initial Commit

---
+++ SystemSounds/SystemSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemSoundsApp.swift
+//  SystemSounds
+//
+//  Created by Jan-Marten on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SystemSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
